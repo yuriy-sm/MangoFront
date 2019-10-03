8d0a9508be9948407e167b5aa0860255afe4cfc9
Yurii Sm <yuriy93smyk@gmail.com>
2019-10-04 00:16:43 +0200

    Add contact us form to the about.html page. When you press submit, browser will generate a JS object that you can check in console

---
+++ main.js
@@ -0,0 +1,9 @@
+function contactUs() {
+    let data1 = {
+        name: document.getElementById("Name").value,
+        email: document.getElementById("Email").value,
+        message: document.getElementById("Message").value
+    };
+       console.log(data1);
+
+    }
