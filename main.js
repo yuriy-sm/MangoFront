5c8e9c7fcd648f50d55a847292e56c3f5677f8a7
Yurii Sm <yuriy93smyk@gmail.com>
2019-10-10 00:22:23 +0200

    created function to view in pade data from converted JSON file. There is need to structure improvments

--- main.js
+++ main.js
@@ -1,32 +1,58 @@
 //contact us form sublission
 function contactUs() {
-    //save data from the form fields in the JS object
-    let data1 = {
-        name: document.getElementById("Name").value,
-        email: document.getElementById("Email").value,
-        message: document.getElementById("Message").value
-    }
-    console.log(data1);
-    document.getElementById("answer").innerHTML = (data1.name + ", Thank you for submittin our form");
+  //save data from the form fields in the JS object
+  let data1 = {
+    name: document.getElementById("Name").value,
+    email: document.getElementById("Email").value,
+    message: document.getElementById("Message").value
+  }
+  console.log(data1);
+  document.getElementById("answer").innerHTML = (data1.name + ", thank you for submittin our form");
 
-    //convert data to the JSON file
-    myJSON = JSON.stringify(data1);
-    console.log(myJSON);
-    //save data to local storage
-    localStorage.setItem("testJSON", myJSON);
+  //convert data to the JSON file
+  myJSON = JSON.stringify(data1);
+  console.log(myJSON);
+  //save data to local storage
+  localStorage.setItem("testJSON", myJSON);
 }
 //attempt to retrive data from the local storage - IT IS NOT WORKING
-        // text = localStorage.getItem("testJSON");
-        // obj = JSON.parse(text);
-        // console.log(obj);
-        // document.getElementById("demo").innerHTML = obj.name;
+// text = localStorage.getItem("testJSON");
+// obj = JSON.parse(text);
+// console.log(obj);
+// document.getElementById("demo").innerHTML = obj.name;
 
 //testing GET method to retrive data from server
 const Http = new XMLHttpRequest();
-const url='https://jsonplaceholder.typicode.com/posts';
+const url = 'https://jsonplaceholder.typicode.com/posts';
 Http.open("GET", url);
 Http.send();
 
 Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
+  var checkData = JSON.parse(Http.responseText);
+  console.log(checkData);
+
+  //List each item in the array - version 1 working
+  // for (let a = 0; a < checkData.length; a++){
+  //   const pe = document.createElement("p");
+  //   pe.innerHTML = (checkData[a].title);
+  //   document.querySelector('#demo').append(pe);
+  //}
+
+  //List each item in the array - version 2 working
+  // for (let key in checkData) {
+  //   const pe = document.createElement("p");
+  // pe.innerHTML = ("Parameter with id "+key+ " have value " +checkData[key].body);
+  // document.querySelector('#demo').append(pe);
+  // }
+
+  //List each item in the array - version 3 working
+  checkData.forEach(showData);
+  function showData(item, index) {
+    const pe = document.createElement("p");
+    pe.innerHTML = ("Id: " + index + "<br>Body: " + item.body + "<br>Item Id: " + item.id + "<br>Title: " + item.title + "<br>User Id: " + item.userId);
+    document.querySelector('#demo').append(pe);
+  }
+
+  // keys for the array elements: item.body, item.id, item.title, item.userId
+
 }
