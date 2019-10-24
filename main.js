a1cb8dfbf389126f3d041b04f922039461b6d61b
Yurii <yuriy93smyk@gmail.com>
2019-10-24 21:59:54 +0200

    In files about and index, created table to preview content

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//contact us form sublission
+//contact us form sublission in about page START
 function contactUs() {
   //save data from the form fields in the JS object
   let data1 = {
@@ -6,53 +6,68 @@ function contactUs() {
     email: document.getElementById("Email").value,
     message: document.getElementById("Message").value
   }
+  //show information from form in console
   console.log(data1);
+  //display confirmation that form submitted
   document.getElementById("answer").innerHTML = (data1.name + ", thank you for submittin our form");
+
+  //add information from the form to table
+  let tableForm = document.getElementById("tableForm");
+  let  row = tableForm.insertRow(1),
+    name = row.insertCell(0),
+    email = row.insertCell(1),
+    message = row.insertCell(2);
+  name.innerHTML = (data1.name);
+  email.innerHTML = (data1.email);
+  message.innerHTML = (data1.message);
+  //create headers for the table
+
 
   //convert data to the JSON file
   myJSON = JSON.stringify(data1);
   console.log(myJSON);
-  //save data to local storage
-  localStorage.setItem("testJSON", myJSON);
 }
-//attempt to retrive data from the local storage - IT IS NOT WORKING
-// text = localStorage.getItem("testJSON");
-// obj = JSON.parse(text);
-// console.log(obj);
-// document.getElementById("demo").innerHTML = obj.name;
 
-//testing GET method to retrive data from server
-const Http = new XMLHttpRequest();
-const url = 'https://jsonplaceholder.typicode.com/posts';
-Http.open("GET", url);
-Http.send();
+//function to recieve data from learning server
+function getData(){
+  //testing GET method to retrive data from server
+  const Http = new XMLHttpRequest();
+  const url = 'https://jsonplaceholder.typicode.com/posts';
+  Http.open("GET", url);
+  Http.send();
+  Http.onreadystatechange = (e) => {
+    var checkData = JSON.parse(Http.responseText);
+    console.log(checkData);
 
-Http.onreadystatechange = (e) => {
-  var checkData = JSON.parse(Http.responseText);
-  console.log(checkData);
-
-  //List each item in the array - version 1 working
-  // for (let a = 0; a < checkData.length; a++){
-  //   const pe = document.createElement("p");
-  //   pe.innerHTML = (checkData[a].title);
-  //   document.querySelector('#demo').append(pe);
-  //}
-
-  //List each item in the array - version 2 working
-  // for (let key in checkData) {
-  //   const pe = document.createElement("p");
-  // pe.innerHTML = ("Parameter with id "+key+ " have value " +checkData[key].body);
-  // document.querySelector('#demo').append(pe);
-  // }
-
-  //List each item in the array - version 3 working
-  checkData.forEach(showData);
-  function showData(item, index) {
-    const pe = document.createElement("p");
-    pe.innerHTML = ("Id: " + index + "<br>Body: " + item.body + "<br>Item Id: " + item.id + "<br>Title: " + item.title + "<br>User Id: " + item.userId);
-    document.querySelector('#demo').append(pe);
+    //List each item in the array - version 3 working
+    checkData.forEach(showData);
+    function showData(item, index) {
+      const pe = document.createElement("p");
+      pe.innerHTML = ("Id: " + index + "<br>Body: " + item.body + "<br>Item Id: " + item.id + "<br>Title: " + item.title + "<br>User Id: " + item.userId);
+      document.querySelector('#demo').append(pe);
+    }
+    // keys for the array elements: item.body, item.id, item.title, item.userId
   }
-
-  // keys for the array elements: item.body, item.id, item.title, item.userId
-
 }
+//***display data from database in the table START
+//projData is located in file data.js
+// ****Create table START******
+// var TableContactForm = '';
+// var rows = 5;
+// var columns = 3;
+// for (var r = 0; r < rows; r++){
+//   TableContactForm += '<tr>';
+//   for (var c = 1; c <= columns; c++){
+//     TableContactForm += '<td>' + c + '</td>';
+//   }
+//   TableContactForm += '</tr>';
+//   TableContactForm += '<tr>';
+//   for (var c = 1; c <= columns; c++){
+//     TableContactForm += '<td>' + c + '</td>';
+//   }
+//   TableContactForm += '</tr>';
+// }
+// document.getElementById("tableContent").innerHTML = document.write('<table id="tableProject">' + TableContactForm + '</table>');
+
+// ****Create table FINISH******
+//***display data from database in the table END
